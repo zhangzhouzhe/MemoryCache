b7dd98b12404a9b1c119ba2ec57a4bd51830c450
zhangzhouzhe <zhangzhouzhe@beisen.com>
2019-10-11 19:48:47 +0800

    InitProj

---
+++ MemoryCache/MemoryCache/CacheEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemoryCache
+{
+    class CacheEntry
+    {
+    }
+    public interface ICacheEntry
+    {
+        object Key { get; set; }
+        object Value { get; set; }
+
+        DateTimeOffset? AbsoluteExpiration { get; set; }
+
+        TimeSpan? SlidingExpiration { get; set; }
+
+        CacheItemPriority Priorty { get; set; }
+
+        long? Size { get; set; }
+    }
+
+    public enum CacheItemPriority
+    {
+        Low,
+        Normal,
+        High,
+        NeverRemover
+    }
+}
---
+++ MemoryCache/MemoryCache/IMemoryCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MemoryCache
+{
+    public interface IMemoryCache
+    {
+        bool TryGetValue(object key, out object value);
+        ICacheEntry CreateEntry(object key);
+        void Remove(object key);
+    }
+}
